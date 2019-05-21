cee503d05277ad76eb39d50a6274fa9cdd7a70c4
distolma <djmakaron2009@gmail.com>
2019-05-21 21:45:14 +0300

    impr: mode rxjs to peer deps. rel(#3)

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-var BehaviorSubject = require('rxjs').BehaviorSubject
-var Subject = require('rxjs').Subject
-var merge = require('rxjs').merge
-var switchMap = require('rxjs/operators').switchMap
-var filter = require('rxjs/operators').filter
+var operators = require('rxjs/operators')
+var rxjs = require('rxjs')
 
 function combineEpics (epics) {
   return function () {
     var args = [].slice.call(arguments)
 
-    return merge.apply(null, epics.map(function (epic) {
+    return rxjs.merge.apply(null, epics.map(function (epic) {
       return epic.apply(null, args)
     }))
   }
 }
 
 function createEpicMiddleware (epics) {
-  var epic$ = new BehaviorSubject(epics)
+  var epic$ = new rxjs.BehaviorSubject(epics)
 
   return function (store) {
-    var action$ = new Subject()
-    var state$ = new Subject()
+    var action$ = new rxjs.Subject()
+    var state$ = new rxjs.Subject()
 
     store.on('@dispatch', function (_, event) {
       action$.next.apply(action$, event)
@@ -27,7 +24,7 @@ function createEpicMiddleware (epics) {
 
     store.on('@changed', function (state) { state$.next(state) })
 
-    var result$ = epic$.pipe(switchMap(function (epic) {
+    var result$ = epic$.pipe(operators.switchMap(function (epic) {
       return epic(action$, state$)
     }))
 
@@ -39,7 +36,7 @@ function ofEvent () {
   var events = [].slice.call(arguments)
 
   return function (source) {
-    return source.pipe(filter(function (event) {
+    return source.pipe(operators.filter(function (event) {
       return events.indexOf(event) !== -1
     }))
   }
